bd99b40566f9f560c0bb9271859c829d5afedf65
zhu <zhu@test.com>
2014-01-15 10:20:20 +0800

    add zeromq server and client

---
+++ zmq/server.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# <nbformat>2</nbformat>
+
+# <codecell>
+
+#
+#   Hello World server in Python
+#   Binds REP socket to tcp://*:5555
+#   Expects "Hello" from client, replies with "World"
+#
+import zmq
+import time
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    #  Wait for next request from client
+    buf = socket.recv()
+    print "I receive:", buf 
+ 
+    if buf == '1':  
+       	socket.send('welcome to server!')  
+    else: 
+        socket.send('bye')
+
